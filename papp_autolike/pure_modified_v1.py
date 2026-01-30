9a3a58cb38990cd8dbe18e7ed0d7125e385a6f37
Dmitriy <dmitriy250300@mail.ru>
2026-01-30 21:45:43 +0300

    30.01.2026

--- papp_autolike/pure_modified_v1.py
+++ papp_autolike/pure_modified_v1.py
@@ -3,7 +3,7 @@ from time import sleep
 from random import randrange
 
 sleep(3)
-like = 'like.png'
+like = 'like2.png'
 geo_accept = 'geo_accept.png'
 quit = 'quit.png'
 reload = 'reload.png'
@@ -14,6 +14,7 @@ while True:
         button_like = pyautogui.locateOnScreen(like, confidence=0.7)
         pyautogui.click(button_like)
         likes += 1
+        pyautogui.moveRel(50, 0, duration=0.5)
         sleep(randrange(10, 22) / 1000)
         if likes % 100 == 0:
             print('Сделано лайков:', likes)
@@ -28,4 +29,4 @@ while True:
                 sleep(5)
             except:
                 pyautogui.scroll(-randrange(400, 600))
-    sleep(0.01)
+    sleep(0.001) # 0.01
